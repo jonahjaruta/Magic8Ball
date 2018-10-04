d6a11c52aa7cf8475665e2c4f3aebadf0279647b
Jonah <jonah.jaruta.2019@student.lhschools.org>
2018-10-04 07:37:48 -0400

    Updated as needed for requirments

--- src/Magic8Ball.java
+++ src/Magic8Ball.java
@@ -3,59 +3,64 @@ import java.util.Scanner;
 public class Magic8Ball {
 
     public static void main(String[]args){
+
         //declare
         Scanner keyboard;
         int testing;
         String question;
+        int pickedNumber;
+        boolean repeat = false;
 
-        //initialization
+        while(repeat == false){
+
+
+            //initialization
         keyboard = new Scanner(System.in);
+        pickedNumber = (int) (Math.random() * 100);
 
         System.out.println("What is your question for the Magic 8 Ball?");
         question = keyboard.nextLine();
-        System.out.println("Alright got it, now input a number between 1 and 100.");
-        testing = keyboard.nextInt();
 
-        if (testing<11 ) {
+        if(pickedNumber<11 ) {
             System.out.println("The outcome looks promising.");
         }
 
-        else if(testing<21){
+        else if(pickedNumber<21){
             System.out.println("I wouldn't bet on it.");
             }
 
-        else if(testing<31){
+        else if(pickedNumber<31){
             System.out.println("Absolutely.");
         }
 
-        else if(testing<41){
+        else if(pickedNumber<41){
             System.out.println("Not now.");
         }
 
-        else if(testing<51){
+        else if(pickedNumber<51){
             System.out.println("Wait for it.");
         }
 
-        else if(testing<61){
+        else if(pickedNumber<61){
             System.out.println("Yes.");
         }
 
-        else if(testing<71){
+        else if(pickedNumber<71){
             System.out.println("Dosen't seem likely");
         }
 
-        else if(testing<81){
+        else if(pickedNumber<81){
             System.out.println("Without a doubt.");
         }
 
-        else if(testing<91){
+        else if(pickedNumber<91){
             System.out.println("No.");
         }
 
-        else if(testing<101){
+        else if(pickedNumber<101){
             System.out.println("Count on it.");
         }
 
-    }
+    }}
 
 }
